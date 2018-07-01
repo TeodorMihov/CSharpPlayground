e90954cd874e7da114645034df12168435477f96
TeodorMihov <tmihov89@gmail.com>
2018-07-01 13:46:51 +0300

    Add multithreading queues in which is possible to setup how many threads will work async.

---
+++ Multithreading/Queues/TaskQueue.cs
@@ -0,0 +1,53 @@
+﻿namespace Multithreading.Queues
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class TaskQueue
+    {
+        private readonly Task[] _workers;
+        private readonly ConcurrentQueue<Action> _testsQueue = new ConcurrentQueue<Action>();
+        private readonly CancellationTokenSource _cancelationToken = new CancellationTokenSource();
+
+        public TaskQueue(int workersCount)
+        {
+            _workers = new Task[workersCount];
+        }
+
+        public void Wait()
+        {
+            for (int i = 0; i < _workers.Length; i++)
+            {
+                _workers[i] = Task.Factory.StartNew(Consume, _cancelationToken.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+            }
+
+            try
+            {
+                Task.WaitAll(_workers);
+            }
+            catch (AggregateException ex)
+            {
+                ex.Handle(e =>
+                {
+                    return e is OperationCanceledException;
+                });
+            }
+        }
+
+        public void Enqueue(Action action)
+        {
+            _testsQueue.Enqueue(action);
+        }
+
+        private void Consume()
+        {
+            Action action;
+            while (_testsQueue.TryDequeue(out action))
+            {
+                action();
+            }
+        }
+    }
+}
---
+++ Multithreading/Queues/ThreadQueue.cs
@@ -0,0 +1,77 @@
+﻿namespace Multithreading.Queues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    public class ThreadQueue
+    {
+        public class ThreadsQueue
+        {
+            private readonly object _lock = new object();
+            private readonly Queue<Action<int>> _actions = new Queue<Action<int>>();
+            private Thread[] _workers;
+
+            public ThreadsQueue(int workerCount)
+            {
+                _workers = new Thread[workerCount];
+
+                // Create and start a separate thread for each worker
+                for (int i = 0; i < workerCount; i++)
+                {
+                    _workers[i] = new Thread(() => Consume(i));
+                    _workers[i].Start();
+                }
+            }
+
+            public void Wait(bool waitForWorkers = true)
+            {
+                // Enqueue one null item per worker to make each exit.
+                foreach (Thread worker in _workers)
+                {
+                    Enqueue(null);
+                }
+
+                // Wait for workers to finish
+                if (waitForWorkers)
+                {
+                    foreach (Thread worker in _workers)
+                    {
+                        worker.Join();
+                    }
+                }
+            }
+
+            public void Enqueue(Action<int> item)
+            {
+                lock (_lock)
+                {
+                    _actions.Enqueue(item);
+                    Monitor.Pulse(_lock);
+                }
+            }
+
+            private void Consume(int i)
+            {
+                while (true)
+                {
+                    Action<int> item;
+                    lock (_lock)
+                    {
+                        while (_actions.Count == 0)
+                        {
+                            Monitor.Wait(_lock);
+                        }
+
+                        item = _actions.Dequeue();
+                    }
+
+                    //This signals our exit.
+                    if (item == null) return;
+
+                    item(i);
+                }
+            }
+        }
+    }
+}
